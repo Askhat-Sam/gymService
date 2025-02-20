7a444ce4a8cb395584e3969142de8188f4d46a64
Askhat <Askhat.Sam@gmail.com>
2025-02-20 21:11:45 +0500

    feat: add TraineeService

---
+++ src/main/java/dev/gymService/service/TraineeService.java
@@ -0,0 +1,13 @@
+package dev.gymService.service;
+
+import dev.gymService.model.Trainee;
+
+import java.util.Date;
+
+public interface TraineeService {
+    Trainee createTrainee(String firstName, String lastName, String userName, String password, Boolean isActive,
+                          Date dateOfBirth, String address, String userId);
+    void updateTrainee(String userId);
+    void deleteTrainee(String userId);
+    Trainee selectTrainee(String userId);
+}

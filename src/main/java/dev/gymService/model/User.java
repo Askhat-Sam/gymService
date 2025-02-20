d7be6553eab04e806c246ff7d0d56199d4468da0
Askhat <Askhat.Sam@gmail.com>
2025-02-20 20:54:47 +0500

    feat: add TrainingType.java and User.java entity

---
+++ src/main/java/dev/gymService/model/User.java
@@ -0,0 +1,70 @@
+package dev.gymService.model;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private Boolean isActive;
+
+    public User(String firstName, String lastName, String userName, String password, Boolean isActive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.isActive = isActive;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
+}

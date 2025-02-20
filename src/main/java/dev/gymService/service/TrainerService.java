4847555fafdf82eeaa0b3560c830661e4446f16e
Askhat <Askhat.Sam@gmail.com>
2025-02-20 21:14:08 +0500

    feat: add TrainerService

---
+++ src/main/java/dev/gymService/service/TrainerService.java
@@ -0,0 +1,9 @@
+package dev.gymService.service;
+
+import dev.gymService.model.Trainer;
+
+public interface TrainerService {
+    Trainer createTrainer(String firstName, String lastName, String userName, String password, Boolean isActive, String specialization, String userId);
+    void updateTrainer(String userId);
+    void selectTrainer(String userId);
+}

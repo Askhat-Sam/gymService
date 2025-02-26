83cce4aa2c7a212d9ab50b4e8068b8ed90c0a2de
Askhat <Askhat.Sam@gmail.com>
2025-02-26 11:12:17 +0500

    feat: add additional logic to UserInformationUtility.generateUserName to cover case of duplication

--- src/main/java/dev/gymService/utills/UserInformationUtility.java
+++ src/main/java/dev/gymService/utills/UserInformationUtility.java
@@ -1,8 +1,11 @@
 package dev.gymService.utills;
 
+import dev.gymService.storage.InMemoryStorage;
+
 import java.util.Random;
 
 public class UserInformationUtility {
+    private static InMemoryStorage inMemoryStorage = null;
     private static final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=<>?";
     private static final int PASSWORD_LENGTH = 10;
     private static final Random random = new Random();
@@ -11,24 +14,34 @@ public class UserInformationUtility {
     private static long trainerId = 0;
     private static long trainingId = 0;
 
-    public static long generateTraineeId(){
+    public UserInformationUtility(InMemoryStorage inMemoryStorage) {
+        this.inMemoryStorage = inMemoryStorage;
+    }
+
+    public static long generateTraineeId() {
         return traineeId++;
     }
 
-    public static long generateTrainerId(){
+    public static long generateTrainerId() {
         return trainerId++;
     }
 
-    public static long generateTrainingId(){
+    public static long generateTrainingId() {
         return trainingId++;
     }
-    public String generateUserName(String firstName, String lastName){
-        return firstName.concat(".").concat(lastName);
+
+    public static String generateUserName(String firstName, String lastName, long id) {
+        String userName = firstName.concat(".").concat(lastName);
+        String finalUserName = userName;
+        if (inMemoryStorage.getTraineeStorage().values().stream().anyMatch(t -> t.getUserName().equals(finalUserName))) {
+            userName = userName.concat(String.valueOf(id));
+        }
+        return userName;
     }
 
-    public String generatePassword(){
+    public static String generatePassword() {
         StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
-        for (int i=0; i<PASSWORD_LENGTH; i++){
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
             int index = random.nextInt(CHARS.length());
             password.append(CHARS.charAt(index));
         }

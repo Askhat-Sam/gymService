d1a5bdb8e851ec1800bdcf751431ee11629f381a
Askhat <Askhat.Sam@gmail.com>
2025-02-26 13:10:19 +0500

    feat: add TrainerDAO

---
+++ src/main/java/dev/gymService/dao/TrainerDAO.java
@@ -0,0 +1,43 @@
+package dev.gymService.dao;
+
+import dev.gymService.model.Trainee;
+import dev.gymService.model.Trainer;
+import dev.gymService.storage.InMemoryStorage;
+import dev.gymService.utills.FileLogger;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Repository
+public class TrainerDAO {
+    private final InMemoryStorage inMemoryStorage;
+    private static final Logger logger = FileLogger.getLogger(TrainerDAO.class);
+
+    public TrainerDAO(InMemoryStorage inMemoryStorage) {
+        this.inMemoryStorage = inMemoryStorage;
+    }
+
+    public Trainer getById(Long id) {
+        logger.info("Getting trainer with id: " + id);
+        return inMemoryStorage.getTrainerStorage().get(id);
+    }
+
+    public void create(Trainer trainer) {
+        logger.info("Creating new trainer with id: " + trainer.getUserId());
+        inMemoryStorage.getTrainerStorage().put(trainer.getUserId(), trainer);
+    }
+    public void update(Trainer trainer) {
+        logger.info("Updating trainer with id: " + trainer.getUserId());
+        inMemoryStorage.getTrainerStorage().put(trainer.getUserId(), trainer);
+    }
+    public void delete(Long id) {
+        logger.info("Deleting trainer with id: " + id);
+        inMemoryStorage.getTrainerStorage().remove(id);
+    }
+    public List<Trainer> getAll(){
+        logger.info("Getting the list of all trainers");
+        return new ArrayList<>(inMemoryStorage.getTrainerStorage().values());
+    }
+}

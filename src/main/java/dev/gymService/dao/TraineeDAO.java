333e4623d3b2180efe164cc6da5baed6e6a7d156
Askhat <Askhat.Sam@gmail.com>
2025-02-26 11:43:36 +0500

    feat: update TraineeDAO to add update/delete/getAll methods and loggers.

--- src/main/java/dev/gymService/dao/TraineeDAO.java
+++ src/main/java/dev/gymService/dao/TraineeDAO.java
@@ -2,56 +2,44 @@ package dev.gymService.dao;
 
 import dev.gymService.model.Trainee;
 import dev.gymService.storage.InMemoryStorage;
+import dev.gymService.utills.FileLogger;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 @Component
-public class TraineeDAO implements Dao{
+public class TraineeDAO {
 
-    private InMemoryStorage inMemoryStorage;
+    private final InMemoryStorage inMemoryStorage;
+    private final String namespace = "trainee";
+    private static final Logger logger = FileLogger.getLogger(TraineeDAO.class);
 
     public TraineeDAO(InMemoryStorage inMemoryStorage) {
         this.inMemoryStorage = inMemoryStorage;
     }
 
-    @Override
     public Trainee getById(Long id) {
-        //Get first name
-        String firstName = (String) inMemoryStorage.getInMemoryStorage().get("trainees").get(id).get(0);
-
-        //Get lastName
-        String lastName = (String) inMemoryStorage.getInMemoryStorage().get("trainees").get(id).get(1);
-
-        //Get userName
-        String userName = (String) inMemoryStorage.getInMemoryStorage().get("trainees").get(id).get(2);
-
-        //Get password
-        String password = (String) inMemoryStorage.getInMemoryStorage().get("trainees").get(id).get(3);
-
-        //Get if Active
-        Boolean isActive = Boolean.valueOf(String.valueOf(inMemoryStorage.getInMemoryStorage().get("trainees").get(id).get(4)));
-
-        //Get birth date
-        LocalDate birthDate = LocalDate.parse(String.valueOf(inMemoryStorage.getInMemoryStorage().get("trainees").get(id).get(5)).trim());
-
-
-        //Get adress
-        String address = (String) inMemoryStorage.getInMemoryStorage().get("trainees").get(id).get(6);
-
-        return new Trainee(firstName, lastName, userName, password, isActive, birthDate, address, id);
+        logger.info("Getting trainee with id: " + id);
+        return inMemoryStorage.getTraineeStorage().get(id);
     }
 
-    @Override
-    public Object save(Object o) {
-        return null;
+    public void create(Trainee trainee) {
+        logger.info("Creating new trainee with id: " + trainee.getUserId());
+        inMemoryStorage.getTraineeStorage().put(trainee.getUserId(), trainee);
+    }
+    public void update(Trainee trainee) {
+        logger.info("Updating trainee with id: " + trainee.getUserId());
+        inMemoryStorage.getTraineeStorage().put(trainee.getUserId(), trainee);
+    }
+    public void delete(Long id) {
+        logger.info("Deleting trainee with id: " + id);
+        inMemoryStorage.getTraineeStorage().remove(id);
+    }
+    public List<Trainee> getAll(){
+        logger.info("Getting the list of all trainees");
+        return new ArrayList<>(inMemoryStorage.getTraineeStorage().values());
     }
 
-    @Override
-    public void delete(Object o) {
-
-    }
 }

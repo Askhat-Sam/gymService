a6202e3667b213285c2611fb30b4752efc21a287
Askhat <Askhat.Sam@gmail.com>
2025-02-26 10:25:22 +0500

    fix: change Trainee constructors parameter sequence

--- src/main/java/dev/gymService/model/Trainee.java
+++ src/main/java/dev/gymService/model/Trainee.java
@@ -9,7 +9,7 @@ public class Trainee extends User {
     private Long userId;
 
 
-    public Trainee(String firstName, String lastName, String userName, String password, Boolean isActive, LocalDate dateOfBirth, String address, Long userId) {
+    public Trainee(Long userId, String firstName, String lastName, String userName, String password, Boolean isActive, LocalDate dateOfBirth, String address) {
         super(firstName, lastName, userName, password, isActive);
         this.dateOfBirth = dateOfBirth;
         this.address = address;
@@ -38,19 +38,5 @@ public class Trainee extends User {
 
     public void setUserId(Long userId) {
         this.userId = userId;
-    }
-
-    @Override
-    public String toString() {
-        return "Trainee{" +
-                "firstName='" + super.getFirstName() +
-                "', lastName='" + super.getLastName() +
-                "', userName='" + super.getUserName() +
-                "', password='" + super.getPassword() +
-                "', isActive='" + super.getActive() +
-                "', dateOfBirth=" + dateOfBirth +
-                "', address='" + address + '\'' +
-                "', userId='" + userId + '\'' +
-                '}';
     }
 }

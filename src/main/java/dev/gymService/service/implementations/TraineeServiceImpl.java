19a99cc3c554bea92b77628705053bee366b511b
Askhat <Askhat.Sam@gmail.com>
2025-02-26 11:51:31 +0500

    feat: update TraineeServiceImpl to add updateTrainee, deleteTrainee, getTraineeById and getAllTrainee methods and loggers.

--- src/main/java/dev/gymService/service/implementations/TraineeServiceImpl.java
+++ src/main/java/dev/gymService/service/implementations/TraineeServiceImpl.java
@@ -3,32 +3,46 @@ package dev.gymService.service.implementations;
 import dev.gymService.dao.TraineeDAO;
 import dev.gymService.model.Trainee;
 import dev.gymService.service.interfaces.TraineeService;
+import dev.gymService.utills.FileLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 public class TraineeServiceImpl implements TraineeService {
     @Autowired
     private TraineeDAO traineeDAO;
 
+    private static final Logger logger = FileLogger.getLogger(TraineeDAO.class);
+
     @Override
-    public Trainee createTrainee(String firstName, String lastName, String userName, String password, Boolean isActive, Date dateOfBirth, String address, String userId) {
-        return null;
+    public void createTrainee(Trainee trainee) {
+        traineeDAO.create(trainee);
     }
 
     @Override
-    public void updateTrainee(String userId) {
-
+    public void updateTrainee(Trainee trainee) {
+        logger.log(Level.INFO, "Calling traineeDAO.update method for trainee with id: " + trainee.getUserId());
+        traineeDAO.update(trainee);
     }
 
     @Override
-    public void deleteTrainee(String userId) {
-        System.out.println("traineer service");
+    public void deleteTrainee(Long id) {
+        logger.log(Level.INFO, "Calling traineeDAO.delete method for trainee with id: " +id);
+        traineeDAO.delete(id);
+    }
+
+    public List<Trainee> getAlTrainee() {
+        logger.log(Level.INFO, "Calling traineeDAO.getAll method");
+        return traineeDAO.getAll();
     }
 
     @Override
-    public Trainee selectTrainee(Long userId) {
-        return traineeDAO.getById(userId);
+    public Trainee getTraineeById(Long id) {
+        logger.log(Level.INFO, "Calling traineeDAO.getById method for trainee with id: " +id);
+        return traineeDAO.getById(id);
     }
 }

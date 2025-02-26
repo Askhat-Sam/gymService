4c720684e94167f2022abc0dbb1853d7adfe11f4
Askhat <Askhat.Sam@gmail.com>
2025-02-26 13:21:17 +0500

    feat: add TrainingServiceImpl to implement TrainingService interface

---
+++ src/main/java/dev/gymService/service/implementations/TrainingServiceImpl.java
@@ -0,0 +1,41 @@
+package dev.gymService.service.implementations;
+
+import dev.gymService.dao.TrainingDAO;
+import dev.gymService.model.Trainer;
+import dev.gymService.model.Training;
+import dev.gymService.service.interfaces.TrainingService;
+
+import java.util.List;
+
+public class TrainingServiceImpl implements TrainingService {
+    private final TrainingDAO trainingDAO;
+
+    public TrainingServiceImpl(TrainingDAO trainingDAO) {
+        this.trainingDAO = trainingDAO;
+    }
+
+    @Override
+    public void createTraining(Training training) {
+        trainingDAO.create(training);
+    }
+
+    @Override
+    public void updateTraining(Training training) {
+        trainingDAO.update(training);
+    }
+
+    @Override
+    public void deleteTraining(Long id) {
+        trainingDAO.delete(id);
+    }
+
+    @Override
+    public Training getTrainingById(Long id) {
+        return trainingDAO.getById(id);
+    }
+
+    @Override
+    public List<Training> getAllTrainings() {
+        return trainingDAO.getAll();
+    }
+}

1cc16d08f1cb55fcccd64b27ca9d63aad37c3c9c
Askhat <Askhat.Sam@gmail.com>
2025-02-27 15:17:55 +0500

    feat: return type for create and update methods and add generateTraineeId() method

--- src/main/java/dev/gymService/dao/TraineeDAO.java
+++ src/main/java/dev/gymService/dao/TraineeDAO.java
@@ -2,44 +2,43 @@ package dev.gymService.dao;
 
 import dev.gymService.model.Trainee;
 import dev.gymService.storage.InMemoryStorage;
-import dev.gymService.utills.FileLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Component
 public class TraineeDAO {
 
     private InMemoryStorage inMemoryStorage;
-    private static final Logger logger = FileLogger.getLogger(TraineeDAO.class);
+
     @Autowired
     public void setInMemoryStorage(InMemoryStorage inMemoryStorage){
         this.inMemoryStorage = inMemoryStorage;
     }
 
     public Trainee getById(Long id) {
-        logger.info("Getting trainee with id: " + id);
         return inMemoryStorage.getTraineeStorage().get(id);
     }
 
-    public void create(Trainee trainee) {
-        logger.info("Creating new trainee with id: " + trainee.getUserId());
+    public Trainee create(Trainee trainee) {
         inMemoryStorage.getTraineeStorage().put(trainee.getUserId(), trainee);
+        return inMemoryStorage.getTraineeStorage().get(trainee.getUserId());
     }
-    public void update(Trainee trainee) {
-        logger.info("Updating trainee with id: " + trainee.getUserId());
+    public Trainee update(Trainee trainee) {
         inMemoryStorage.getTraineeStorage().put(trainee.getUserId(), trainee);
+        return inMemoryStorage.getTraineeStorage().get(trainee.getUserId());
     }
     public void delete(Long id) {
-        logger.info("Deleting trainee with id: " + id);
         inMemoryStorage.getTraineeStorage().remove(id);
     }
     public List<Trainee> getAll(){
-        logger.info("Getting the list of all trainees");
         return new ArrayList<>(inMemoryStorage.getTraineeStorage().values());
     }
 
+    public long generateTraineeId() {
+        long maxNumber = getAll().size();
+        return ++maxNumber;
+    }
 }

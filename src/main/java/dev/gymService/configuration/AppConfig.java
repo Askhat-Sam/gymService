af28c1436f586280d0b908f07a6e6f49df425930
Askhat <Askhat.Sam@gmail.com>
2025-02-28 10:52:22 +0500

    feat: perform static import of @Configuration annotation

--- src/main/java/dev/gymService/configuration/AppConfig.java
+++ src/main/java/dev/gymService/configuration/AppConfig.java
@@ -1,8 +1,9 @@
 package dev.gymService.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 @ComponentScan(basePackages = "dev.gymService")
 public class AppConfig {
 

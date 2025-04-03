978f973876d4cac6f69dc29f9f1a152b7d97bc4b
Askhat <Askhat.Sam@gmail.com>
2025-04-03 21:04:11 +0500

    fix: Correct logger in UserService

--- src/main/java/dev/gymService/service/interfaces/UserService.java
+++ src/main/java/dev/gymService/service/interfaces/UserService.java
@@ -2,16 +2,18 @@ package dev.gymService.service.interfaces;
 
 import dev.gymService.exception.UserNotAuthenticatedException;
 import dev.gymService.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface UserService {
-//    static final Logger logger = FileLogger.getLogger(TraineeRepositoryImpl.class);
+    final Logger logger = LoggerFactory.getLogger("UserService");
 
     default boolean isAuthenticated(String userName, String password, User user) {
         if (user.getUserName().equals(userName) && user.getPassword().equals(password)) {
-//            logger.log(Level.INFO, "Successful authentication for trainee: " + userName);
+            logger.info("Successful authentication for trainee: " + userName);
             return true;
         } else {
-//            logger.log(Level.INFO, "Failed authentication for trainee: " + userName);
+            logger.info("Failed authentication for trainee: " + userName);
             throw new UserNotAuthenticatedException("Failed to authenticate the user: [" + userName + "]");
         }
     }

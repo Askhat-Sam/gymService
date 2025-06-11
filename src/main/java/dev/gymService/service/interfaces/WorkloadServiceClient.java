8273e82c2efab03da29cc15f3b4b471032e7f79b
Askhat <Askhat.Sam@gmail.com>
2025-06-11 22:23:15 +0500

    fix: Remove unused imports

--- src/main/java/dev/gymService/service/interfaces/WorkloadServiceClient.java
+++ src/main/java/dev/gymService/service/interfaces/WorkloadServiceClient.java
@@ -1,8 +1,6 @@
 package dev.gymService.service.interfaces;
 
 import dev.gymService.model.*;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 public interface WorkloadServiceClient {
     String updateMicroservice(Training training, ActionType actionType, Trainee trainee, Trainer trainer);
